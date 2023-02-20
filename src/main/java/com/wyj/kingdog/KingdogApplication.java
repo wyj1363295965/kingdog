49b48d00272a0ba6e9079ba28dfe1940955693fe
wyj <15927717572@163.com>
2023-02-20 15:22:54 +0800

    project init

---
+++ src/main/java/com/wyj/kingdog/KingdogApplication.java
@@ -0,0 +1,13 @@
+package com.wyj.kingdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingdogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyj/kingdog/KingdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyj.kingdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
