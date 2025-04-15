b44d2da078c8def8707fc16bca66a20fe1f71aec
王亚杰 <1363295965@qq.com>
2025-04-15 11:43:22 +0800

    nginx测试

--- src/main/java/com/wyj/kingdog/schedule/MyRunner.java
+++ src/main/java/com/wyj/kingdog/schedule/MyRunner.java
@@ -1,17 +1,21 @@
 package com.wyj.kingdog.schedule;
 
 import com.wyj.kingdog.config.tcp.BootNettyServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyRunner implements CommandLineRunner {
 
+    @Value("${netty.port}")
+    private Integer nettyPort;
+
     @Override
     public void run(String... args) throws Exception {
         /**
          * 使用异步注解方式启动netty服务端服务
          */
-        new BootNettyServer().bind(8888);
+        new BootNettyServer().bind(nettyPort);
     }
 }
